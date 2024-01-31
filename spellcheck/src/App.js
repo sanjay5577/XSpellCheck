cab780a4f5e7dfa61d29c5c9d34996fe342dd508
sanjay5577 <143835637+sanjay5577@users.noreply.github.com>
2024-01-31 16:29:12 +0000

    final

---
+++ spellcheck/src/App.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import './App.css';
+
+// Define a custom dictionary of words and their corrections
+const customDictionary = {
+  teh: "the",
+  wrok: "work",
+  fot: "for",
+  exampl: "example",
+};
+
+function App() {
+const[inputText , setInputText] = useState('');
+const[suggestedText, setSuggestedText] = useState('');
+
+const handleInputChange=(e)=>{
+  const text =e.target.value;
+  setInputText(text);
+
+  const words = text.split(" ");
+
+
+  const correctedwords = words.map((word)=>{
+    const correctword = customDictionary[word];
+    return correctword || word;
+  })
+
+  const firstcorrectedword = correctedwords.find((word, index)=>{
+      return  word!==words[index];
+  })
+
+  setSuggestedText(firstcorrectedword) 
+
+}
+
+  return (
+    <div className="App">
+      <h1>Spell Check and Auto-Correction</h1>
+        <textarea
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Enter text..."
+          rows={5}
+          cols={40}
+        />
+        {suggestedText && (
+          <p>
+            Did you mean: <strong>{suggestedText}</strong>?
+          </p>
+        )}
+    </div>
+  );
+}
+
+export default App;
